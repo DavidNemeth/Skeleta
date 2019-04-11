a46d175b9fd853285c2e25a7218c70bd95a18e15
David Nemeth <david.nemeth90@gmail.com>
2019-04-11 13:53:35 +0200

    releaseId to taskitem

---
+++ Skeleta/Migrations/20190411114927_task_releaseId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Skeleta.Migrations
+{
+    public partial class task_releaseId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReleaseId",
+                table: "TaskItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReleaseId",
+                table: "TaskItems");
+        }
+    }
+}
--- Skeleta/ViewModels/WorkItemViewModels/TaskListViewModel.cs
+++ Skeleta/ViewModels/WorkItemViewModels/TaskListViewModel.cs
@@ -13,6 +13,7 @@ namespace Skeleta.ViewModels.WorkItemViewModels
 		public string Title { get; set; }
 		public string Priority { get; set; }
 		public string Status { get; set; }
+		public string ReleaseId { get; set; }
 		public AssignUserViewModel Developer { get; set; }
 		public AssignUserViewModel Tester { get; set; }
 
